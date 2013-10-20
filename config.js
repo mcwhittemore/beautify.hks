3e0d81610eb935d6a74f0298789f58dee82bda1c
Matthew Chase Whittemore <matthew@socialtables.com>
2013-10-20 02:53:06 -0400

    config is now up and running... but is it really working

--- config.js
+++ config.js
@@ -1,53 +1,59 @@
 var hksConfig = require("hooks-config");
 hksConfig.hookModule = "beautify.hks";
+hksConfig.cwd = process.cwd()+"/../..";
 
 var read = require("read");
 
 var baseConfig = require("./beautify_defaults.json");
 
-hksConfig.view(function(err, userConfig){
+hksConfig.view(function(err, userConfig) {
 
-	if(err){
-		console.log(err);
-	}
-	else{
-		var keys = Object.keys(baseConfig);
-		var config = {};
+    if (err) {
+        console.log(err);
+    } else {
+        var keys = Object.keys(baseConfig);
+        var config = {};
 
-		var keyToPhrase = function(key){
-			return key.split("_").map(function(word){
-				word[0] = word[0].toUpperCase();
-				return word;
-			}).join(" ");
-		}
+        var keyToPhrase = function(key) {
+            return key.split("_").map(function(word) {
+                word[0] = word[0].toUpperCase();
+                return word;
+            }).join(" ");
+        }
 
-		var ask = function(i){
-			if(i==keys.length){
-				done();
-			}
-			else{
-				var opts = {
-					prompt: keyToPhrase(keys[i])+":",
-					default: (userConfig[keys[i]] || baseConfig[keys[i]])+""
-				}
-				read(opts, function(err, value){
-					if(err){
-						console.log("User forced exit");
-						process.exit(0);
-					}
-					else{
-						config[keys[i]] = value;
-						ask(i+1);
-					}
-				});
-			}
-		}
+        var ask = function(i) {
+            if (i == keys.length) {
+                done();
+            } else {
+                var opts = {
+                    prompt: keyToPhrase(keys[i]) + ":",
+                    default: (userConfig[keys[i]] || baseConfig[keys[i]]) + ""
+                }
+                read(opts, function(err, value) {
+                    if (err) {
+                        console.log("User forced exit");
+                        process.exit(0);
+                    } else {
+                        if (value === "true") {
+                            value = true;
+                        } else if (value === "false") {
+                            value = false;
+                        } else if (parseInt(value) == value) {
+                            value = parseInt(value);
+                        }
 
-		var done = function(){
-			console.log(config);
-		}
+                        config[keys[i]] = value;
+                        ask(i + 1);
+                    }
+                });
+            }
+        }
 
-		ask(0);
-	}
+        var done = function() {
+            hksConfig.save(config);
+        }
+
+        ask(0);
+    }
 
 });
--- index.js
+++ index.js
@@ -3,74 +3,84 @@ var colors = require("colors");
 var exec = require("child_process").exec;
 var fs = require("fs");
 var sgf = require("staged-git-files");
+var hksConfig = require("hooks-config");
+hksConfig.hookModule = "beautify.hks";
 
 var hook = process.argv[2];
 
 if (hook != "pre-commit") {
     console.error("This is only going to work with pre-commit");
 } else {
-    
-    sgf("AMR", function(err, results){
-        if (err) {
-            console.log("GIT ERROR".red);
-            console.log(err);
-            process.exit(1);
-        } else if (results.length == 0) {
-            console.log("> No files to commit".yellow);
-        } else {
-            var iFiles = results.length;
-            var files_to_add = [];
-            while (iFiles--) {
-                var filename = results[iFiles].filename;
-                var found = filename.match(/\.js$/g);
-                if (found) {
-                    var ugly;
-                    try {
-                        ugly = fs.readFileSync(sgf.cwd+"/"+filename, {encoding: "utf8"});
-                    } catch (err) {
-                        console.log(filename + ": ERROR".red);
-                        console.log(err.message);
-                        process.exit(1);
-                    }
-
-                    var beautiful = beautify(ugly);
-                    if (ugly != beautiful) {
-                        fs.writeFileSync(filename, beautiful);
-                        console.log(filename + ": BEAUTIFIED".blue);
-                        files_to_add.push(filename);
-                    } else {
-                        console.log(filename + ": ALREADY BEAUTIFUL".green);
-                    }
-                } else {
-                    console.log(filename + ": SKIPPED".yellow);
-                }
-            }
-            if (files_to_add.length > 0) {
-                //TODO: it would be sweet if hooks had an API that just added this in.
-                var adder = function(iFiles) {
-                    exec("git add " + files_to_add[iFiles], function(err, stdout, stderr) {
-                        if (err) {
-                            console.log("GIT ADD ERROR".red);
-                            console.log(err.message);
-                            process.exit(1);
-                        } else if (stderr) {
-                            console.log("GIT ADD ERROR".red);
-                            console.log(err.message);
-                            process.exit(1);
-                        } else {
-                            if (iFiles > 0) {
-                                adder(iFiles - 1);
-                            } else {
-                                console.log("ALL CHANGES ADDED!");
-                            }
-                        }
-                    });
-                }
-
-                adder(files_to_add.length - 1);
-            } else {
-                console.log("YOU WRITE BEAUTIFUL CODE!");
-            }
+    hksConfig.view(function(err, config) {
+        console.log(config);
+        if (JSON.stringify(config) === "{}") {
+            config = require("./beautify_defaults.json");
         }
+        sgf("AMR", function(err, results) {
+            if (err) {
+                console.log("GIT ERROR".red);
+                console.log(err);
+                process.exit(1);
+            } else if (results.length == 0) {
+                console.log("> No files to commit".yellow);
+            } else {
+                var iFiles = results.length;
+                var files_to_add = [];
+                while (iFiles--) {
+                    var filename = results[iFiles].filename;
+                    var found = filename.match(/\.js$/g);
+                    if (found) {
+                        var ugly;
+                        try {
+                            ugly = fs.readFileSync(sgf.cwd + "/" + filename, {
+                                encoding: "utf8"
+                            });
+                        } catch (err) {
+                            console.log(filename + ": ERROR".red);
+                            console.log(err.message);
+                            process.exit(1);
+                        }
+
+                        var beautiful = beautify(ugly, config);
+                        if (ugly != beautiful) {
+                            fs.writeFileSync(filename, beautiful);
+                            console.log(filename + ": BEAUTIFIED".blue);
+                            files_to_add.push(filename);
+                        } else {
+                            console.log(filename + ": ALREADY BEAUTIFUL".green);
+                        }
+                    } else {
+                        console.log(filename + ": SKIPPED".yellow);
+                    }
+                }
+                if (files_to_add.length > 0) {
+                    //TODO: it would be sweet if hooks had an API that just added this in.
+                    var adder = function(iFiles) {
+                        exec("git add " + files_to_add[iFiles], function(err, stdout, stderr) {
+                            if (err) {
+                                console.log("GIT ADD ERROR".red);
+                                console.log(err.message);
+                                process.exit(1);
+                            } else if (stderr) {
+                                console.log("GIT ADD ERROR".red);
+                                console.log(err.message);
+                                process.exit(1);
+                            } else {
+                                if (iFiles > 0) {
+                                    adder(iFiles - 1);
+                                } else {
+                                    console.log("ALL CHANGES ADDED!");
+                                }
+                            }
+                        });
+                    }
+
+                    adder(files_to_add.length - 1);
+                } else {
+                    console.log("YOU WRITE BEAUTIFUL CODE!");
+                }
+            }
+        });
     });
+
 }
